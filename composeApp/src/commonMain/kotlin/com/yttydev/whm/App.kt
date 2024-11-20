3c773b3c5ab24db8088a066cab7b82752bc5c3f1
Yuriy Titov <yurtitov@gmail.com>
2024-11-20 21:28:57 +0200

    [WHM-11] Add BottomNavigationView - Implement bottomNavBar

--- composeApp/src/commonMain/kotlin/com/yttydev/whm/App.kt
+++ composeApp/src/commonMain/kotlin/com/yttydev/whm/App.kt
@@ -1,17 +1,31 @@
 package com.yttydev.whm
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.yttydev.whm.di.koinConfig
 import com.yttydev.whm.navigation.Destination
 import com.yttydev.whm.navigation.NavHost
 import com.yttydev.whm.navigation.NavigationIntent
+import com.yttydev.whm.navigation.TopLevelRoute
 import com.yttydev.whm.navigation.composable
 import com.yttydev.whm.presentation.exercises.ExercisesScreen
 import com.yttydev.whm.presentation.practice.PracticeScreen
@@ -32,15 +46,54 @@ fun App() {
         val appViewModel = koinViewModel<AppViewModel>()
         val navController = rememberNavController()
 
+        val topLevelRoutes = listOf(
+            TopLevelRoute(
+                name = "Exercises",
+                destination = Destination.ExercisesScreen,
+                icon = Icons.Outlined.AccountBox
+            ),
+            TopLevelRoute(
+                name = "Statistics",
+                destination = Destination.StatisticsScreen,
+                icon = Icons.Outlined.Info
+            ),
+            TopLevelRoute(
+                name = "Profile",
+                destination = Destination.ProfileScreen,
+                icon = Icons.Outlined.Person
+            ),
+        )
+
         NavigationEffects(
             navigationChannel = appViewModel.navigationChannel,
             navHostController = navController
         )
 
         MaterialTheme {
-            Surface(
+            Scaffold(
                 modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colors.background
+                bottomBar = {
+                    BottomNavigation {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+                        topLevelRoutes.forEach { topLevelRoute ->
+                            BottomNavigationItem(
+                                icon = { Icon(topLevelRoute.icon, contentDescription = topLevelRoute.name) },
+                                label = { Text(topLevelRoute.name) },
+                                selected = currentDestination?.hierarchy?.any { it.hasRoute(topLevelRoute.destination.fullRoute::class) } == true,
+                                onClick = {
+                                    navController.navigate(topLevelRoute.destination.fullRoute) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
             ) {
                 NavHost(
                     navController = navController,
--- composeApp/src/commonMain/kotlin/com/yttydev/whm/presentation/exercises/ExercisesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yttydev/whm/presentation/exercises/ExercisesScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 import wimhofmethod.composeapp.generated.resources.Res
@@ -34,33 +32,6 @@ fun ExercisesScreen() {
                 Image(painterResource(Res.drawable.compose_multiplatform), null)
                 Text("Compose")
             }
-        }
-
-        Button(
-            onClick = vm::onNavigateToPractice,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 8.dp)
-        ) {
-            Text("Practice")
-        }
-
-        Button(
-            onClick = vm::onNavigateToStatistics,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 8.dp)
-        ) {
-            Text("Statistics")
-        }
-
-        Button(
-            onClick = vm::onNavigateToProfile,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 8.dp)
-        ) {
-            Text("Profile")
         }
     }
 }
