9dd27776bf1312039e90edcaa0f4282991ae5fdb
Yuriy Titov <yurtitov@gmail.com>
2024-11-30 08:30:42 +0200

    [WHM-22] ExercisesScreen - Implement basic ExercisesScreen

--- composeApp/src/commonMain/kotlin/com/yttydev/whm/presentation/exercises/ExercisesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yttydev/whm/presentation/exercises/ExercisesScreen.kt
@@ -1,37 +1,66 @@
 package com.yttydev.whm.presentation.exercises
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
-import wimhofmethod.composeapp.generated.resources.Res
-import wimhofmethod.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun ExercisesScreen() {
     val vm = koinViewModel<ExercisesViewModel>()
-    var showContent by remember { mutableStateOf(false) }
-    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Exercises screen")
-        Button(onClick = { showContent = !showContent }) {
-            Text("111")
-        }
-        AnimatedVisibility(showContent) {
-            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-                Text("Compose")
-            }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            StartButton(
+                text = "Breathing"
+            )
+            StartButton(text = "Cold shower")
+            StartButton(text = "Explore exercises")
         }
     }
+
+}
+
+@Composable
+private fun StartButton(
+    text: String,
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        elevation = ButtonDefaults.elevation(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun StartButtonPreview() {
+    StartButton(
+        text = "Some button"
+    )
 }
